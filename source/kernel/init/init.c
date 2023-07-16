805e0c7917c45c8b41060d08aa0299dc4c5525b8
Rafael <simon_yolo@163.com>
2023-07-16 15:02:29 +0800

    实现链表查询函数

--- source/kernel/init/init.c
+++ source/kernel/init/init.c
@@ -38,6 +38,9 @@ void list_test() {
     list_t list;
 
     list_init(&list);
+    log_printf("list: first=0x%x, last=0x%x, count=%d",
+        list_first(&list), list_last(&list), list_count(&list));
+    
 }
 
 void init_main (void) {
