2b8a55f72c3794a77f522c997bdba180b3844501
Rafael <simon_yolo@163.com>
2023-07-23 20:55:26 +0800

    位图位的分配

--- source/kernel/include/tools/bitmap.h
+++ source/kernel/include/tools/bitmap.h
@@ -11,5 +11,9 @@ typedef struct _bitmap_t {
 
 int bitmap_byte_count(int bit_count);
 void bitmap_init (bitmap_t * bitmap, uint8_t * bits, int count, int init_bit);
+int bitmap_get_bit (bitmap_t * bitmap, int index);
+void bitmap_set_bit (bitmap_t * bitmap, int index, int count,int bit);
+int bitmap_is_set (bitmap_t * bitmap, int index);   // 判断某一位是否置0还是置1
+int bitmap_alloc_nbits (bitmap_t * bitmap, int bit, int count);     // 找连续的多个位为bit的位进行取反
 
 #endif
--- source/kernel/tools/bitmap.c
+++ source/kernel/tools/bitmap.c
@@ -11,7 +11,55 @@ void bitmap_init (bitmap_t * bitmap, uint8_t * bits, int count, int init_bit) {
     bitmap->bit_count = count;
     bitmap->bits = bits;
 
-    int bytes = bitmap_byte_count(itmap->bit_count);    // 计算位图需要多少字节表示
-    kelnel_memset(bitmap->bits, init_bit ? 0xFF : 0, bytes);
+    int bytes = bitmap_byte_count(bitmap->bit_count);    // 计算位图需要多少字节表示
+    kernel_memset(bitmap->bits, init_bit ? 0xFF : 0, bytes);
+
+}
+
+int bitmap_get_bit (bitmap_t * bitmap, int index) {
+    return bitmap->bits[index / 8] & (1 << (index % 8));
+}
+
+void bitmap_set_bit (bitmap_t * bitmap, int index, int count,int bit) {
+    for (int i = 0; (i < count) && (index < bitmap->bit_count); i ++ , index++) {
+        if (bit) {
+            bitmap->bits[index / 8] |= (1 << (index % 8));
+        } else {
+            bitmap->bits[index / 8] &= ~(1 << (index % 8));
+        }
+    }
+}
+
+int bitmap_is_set (bitmap_t * bitmap, int index) {
+    return bitmap_get_bit(bitmap, index) ? 1 : 0;
+}
+
+int bitmap_alloc_nbits (bitmap_t * bitmap, int bit, int count) {
+    int search_idx = 0;
+    int ok_index = -1;
+
+    while (search_idx < bitmap->bit_count) {
+        if (bitmap_get_bit(bitmap, search_idx) != bit) {
+            search_idx++;
+            continue;
+        }
+
+        ok_index = search_idx;
+        int i;
+        for (i = 1; (i < count) && (search_idx < bitmap->bit_count); i++ ) {
+            if (bitmap_get_bit(bitmap, search_idx++) != bit) {
+                ok_index = -1;
+                break;
+            }
+        }
+
+        if (i >= count) {
+            bitmap_set_bit(bitmap, ok_index, count, ~bit);
+            return ok_index;
+        }
+
+    }
+
+    return -1;
 
 }
