989570bca52413a8ff410d9ab5fef188ab3c71af
Rafael <simon_yolo@163.com>
2023-08-16 23:48:11 +0800

    echo命令的实现

--- source/shell/main.c
+++ source/shell/main.c
@@ -3,6 +3,7 @@
 #include "string.h"
 #include "stdio.h"
 #include "main.h"
+#include <getopt.h>
 
 
 static cli_t cli;
@@ -24,6 +25,59 @@ static int do_clear (int argc, char **argv) {
     return 0;
 }
 
+// 回显操作
+static int do_echo (int argc, char **argv) {
+     // 只有一个参数，需要先手动输入，再输出
+    if (argc == 1) {
+        char msg_buf[128];
+
+        fgets(msg_buf, sizeof(msg_buf), stdin);
+        msg_buf[sizeof(msg_buf) - 1] = '\0';
+        puts(msg_buf);
+        return 0;
+    }
+
+    int count = 1;
+    int ch;
+    while ((ch = getopt(argc, argv, "n:h")) != -1) {
+        switch (ch)
+        {
+        case 'h':
+            // 有参数 -h
+            puts("echo any message");
+            puts("Usage: echo [-n count] message");
+            optind = 1;  
+            return 0;
+        case 'n':
+            // 有 -n num
+            count = atoi(optarg);
+            break;
+        case '?':
+            if (optarg) {
+                fprintf(stderr, "Unknown option: -%s\n", optarg);
+            }
+            optind = 1;  
+            return -1;
+        default:
+            break;
+        }
+    }
+
+    if (optind > argc - 1) {
+        fprintf(stderr, "Message is empty\n");
+        optind = 1;  
+        return -1;
+    }
+
+    char * msg = argv[optind];
+    for (int i = 0; i < count; i++) {
+        puts(msg);
+    }
+    optind = 1;  
+    return 0;
+
+}
+
 static const cli_cmd_t cmd_list[] = {
     {
         .name = "help",
@@ -34,6 +88,11 @@ static const cli_cmd_t cmd_list[] = {
         .name = "clear",
         .usage = "clear -- clear screen",
         .do_func = do_clear,
+    },
+    {
+        .name = "echo",
+        .usage = "echo [-n count] msg -- echo something",
+        .do_func = do_echo,
     },
 };
 
