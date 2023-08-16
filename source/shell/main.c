c10e23bd61c9d1e6f3e508289569d9a2295da65b
Rafael <simon_yolo@163.com>
2023-08-16 21:12:48 +0800

    十九、实现一个命令行解释器---初始化命令行解释器

--- source/shell/main.c
+++ source/shell/main.c
@@ -1,44 +1,47 @@
 #include "lib_syscall.h"
+#include "stdlib.h"
+#include "string.h"
 #include "stdio.h"
+#include "main.h"
 
-char cmd_buf[256];
+
+static cli_t cli;
+static const char * promot = "sh >>";
+
+static int do_help (int argc, char **argv) {
+    return 0;
+}
+
+static const cli_cmd_t cmd_list[] = {
+    {
+        .name = "help",
+        .usage = "help -- list supported command",
+        .do_func = do_help,
+    },
+};
+
+// 显示命令行提示符
+static void show_promot (void) {
+    printf("%s", cli.promot);
+    fflush(stdout);
+}
+
+static void cli_init (const char * promot, const cli_cmd_t * cmd_list, int size) {
+    cli.promot = promot;
+    memset(cli.curr_input, 0, CLI_INPUT_SIZE);
+    cli.cmd_start = cmd_list;
+    cli.cmd_end = cmd_list + size;
+}
+
 int main(int argc, char ** argv) {
-#if 0
-    sbrk(0);
-    sbrk(100);
-    sbrk(200);
-    sbrk(4096*2 + 200);
-    sbrk(4096*5 + 1234);
-
-    printf("abef\b\b\b\bcd\n");     // cdef  \b 是光标左移1位，
-    printf("abcd\x7f;fg\n");        // abc;fg
-    printf("\0337Hello, world!\0338123\n");   // 123lo,world
-    printf("\033[31;42mHello,world!\033[39;49m123\n");
-
-    printf("123\033[2DHello,word!\n");  // 光标左移2，1Hello,word!
-    printf("123\033[2CHello,word!\n");  // 光标右移2，123  Hello,word!
-
-    printf("\033[31m\n");  // ESC [pn m, Hello,world红色，其余绿色
-    printf("\033[10;10H test!\n");  // 定位到10, 10，test!
-    printf("\033[20;20H test!\n");  // 定位到20, 20，test!
-    printf("\033[32;25;39m123\n");  // ESC [pn m, Hello,world红色，其余绿色 
-
-    printf("\033[2J\n");   // clear screen
-#endif
     open(argv[0], 0);           // int fd = 0   stdin 三个都是指向同一个tty设备，tty0，只是在进程的 文件表中，fd不一样。  => tty0
     dup(0);                     // int fd = 1   stdout  => tty0
     dup(0);                     // int fd = 2   stdeer  => tty0
 
-    printf("Hello from shell\n");
-    printf("OsTest\n");
-    fprintf(stderr, "error");
-
+    
+    cli_init(promot, cmd_list, sizeof(cmd_list) / sizeof(cmd_list[0]));
     for (;;) {
-        gets(cmd_buf);
-        puts(cmd_buf);
-
-        // printf("shell pid=%d\n", getpid());
-        // msleep(1000);
-
+        show_promot();
+        gets(cli.curr_input);
     }
 }
